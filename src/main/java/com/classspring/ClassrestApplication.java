f4260d6bc818c00705ba80afcc740f9275562600
ammy <sam.wangui13@gmail.com>
2018-04-19 15:12:08 +0300

    InitialCommit

--- src/main/java/com/classspring/ClassrestApplication.java
+++ src/main/java/com/classspring/ClassrestApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ClassrestApplication {
 
 	public static void main(String[] args) {
+		//springboot
 		SpringApplication.run(ClassrestApplication.class, args);
 	}
 }
